18ef1e1fb8114a0aaf71b93ed14d5ab7eda1f642
Linkarii <realblazelink@gmail.com>
2026-05-03 16:57:09 -0400

    Update page.js

--- app/page.js
+++ app/page.js
@@ -93,7 +93,7 @@ export default function Home() {
       />
 
       {/* Grid */}
-      <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
+      <div className="grid gap-8 sm:grid-cols-1 md:grid-cols-2">
         {filtered.map((game) => (
           <GameCard
             key={game.id}
