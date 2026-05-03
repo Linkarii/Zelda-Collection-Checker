e7f05f16a24df3f47d4e88ea1634fa8924141e11
Linkarii <realblazelink@gmail.com>
2026-05-03 16:25:16 -0400

    Update page.js

---
+++ page.js
@@ -0,0 +1,29 @@
+export const initialGames = [
+  {
+    id: 1,
+    title: "Ocarina of Time",
+    platform: "N64",
+    year: 1998,
+    owned: false,
+    condition: "",
+    image: "https://upload.wikimedia.org/wikipedia/en/5/57/The_Legend_of_Zelda_Ocarina_of_Time.jpg",
+  },
+  {
+    id: 2,
+    title: "Majora's Mask",
+    platform: "N64",
+    year: 2000,
+    owned: false,
+    condition: "",
+    image: "https://upload.wikimedia.org/wikipedia/en/2/2a/The_Legend_of_Zelda_Majora%27s_Mask_Cover.jpg",
+  },
+  {
+    id: 3,
+    title: "Breath of the Wild",
+    platform: "Switch",
+    year: 2017,
+    owned: false,
+    condition: "",
+    image: "https://upload.wikimedia.org/wikipedia/en/a/a9/The_Legend_of_Zelda_Breath_of_the_Wild.jpg",
+  },
+];
